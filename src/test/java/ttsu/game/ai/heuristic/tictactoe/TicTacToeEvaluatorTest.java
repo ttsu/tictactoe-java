10267f7fd3baf1e3fb2c233b21555ec89e2be414
Tim Tsu <timtsu@gmail.com>
2011-12-26 20:15:27 -0600

    fix and improve evaluator test
    TicTacToe evaluator test for win expected the incorrect score.
    Added test for computing the winning score based on the amount available states.

--- src/test/java/ttsu/game/ai/heuristic/tictactoe/TicTacToeEvaluatorTest.java
+++ src/test/java/ttsu/game/ai/heuristic/tictactoe/TicTacToeEvaluatorTest.java
@@ -3,6 +3,8 @@ package ttsu.game.ai.heuristic.tictactoe;
 import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,10 +13,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import ttsu.game.ai.heuristic.tictactoe.TicTacToeEvaluator;
+import ttsu.game.DiscreteGameState;
 import ttsu.game.tictactoe.TicTacToeGameState;
 import ttsu.game.tictactoe.TicTacToeGameState.Player;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class TicTacToeEvaluatorTest {
@@ -22,6 +23,8 @@ public class TicTacToeEvaluatorTest {
     private TicTacToeEvaluator evaluator;
     @Mock
     private TicTacToeGameState game;
+    @Mock
+    private List<DiscreteGameState> availableStates;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -39,21 +42,29 @@ public class TicTacToeEvaluatorTest {
         thrown.expectMessage("player cannot be null");
         new TicTacToeEvaluator(null);
     }
-    
+
     // -- evaluate
-    
+
     @Test
     public void evaluateWin() {
         when(game.hasWin(Player.X)).thenReturn(true);
-        assertThat(evaluator.evaluate(game)).isEqualTo(100);
+        assertThat(evaluator.evaluate(game)).isEqualTo(1);
     }
-    
+
+    @Test
+    public void evaluateWinConsidersAvailableMoves() {
+        when(game.hasWin(Player.X)).thenReturn(true);
+        when(game.availableStates()).thenReturn(availableStates);
+        when(availableStates.size()).thenReturn(5);
+        assertThat(evaluator.evaluate(game)).isEqualTo(6);
+    }
+
     @Test
     public void evaluateLoss() {
         when(game.hasWin(Player.O)).thenReturn(true);
         assertThat(evaluator.evaluate(game)).isEqualTo(-1);
     }
-    
+
     @Test
     public void evaluateDraw() {
         assertThat(evaluator.evaluate(game)).isEqualTo(0);
