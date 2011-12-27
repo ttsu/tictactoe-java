b9526c3f1ac0aefe48d74f53d6c5952d4e5631cf
Tim Tsu <timtsu@gmail.com>
2011-12-26 18:28:17 -0600

    refactor console app
    Broke up the main function and added input validation messages.

--- src/main/java/ttsu/game/tictactoe/TicTacToeMain.java
+++ src/main/java/ttsu/game/tictactoe/TicTacToeMain.java
@@ -14,30 +14,85 @@ public class TicTacToeMain {
      * @param args
      */
     public static void main(String[] args) {
+
+        printInstructions();
+
         TicTacToeGameState game = new TicTacToeGameState();
         TicTacToeEvaluator eval = new TicTacToeEvaluator(Player.O);
         GameIntelligenceAgent<TicTacToeGameState> agent = new MinimaxAgent<TicTacToeGameState>(eval);
         Scanner scanner = new Scanner(System.in);
+
         while (!game.isOver()) {
-            System.out.println("Player " + game.getCurrentPlayer() + ":");
-            int row, col;
+            game.print();
+            userPlay(scanner, game);
+            game.switchPlayer();
+            computerPlay(agent, game);
+            game.switchPlayer();
+        }
+        game.print();
+        printGameOver(game);
+    }
+
+    private static void computerPlay(GameIntelligenceAgent<TicTacToeGameState> agent, TicTacToeGameState game) {
+        TicTacToeGameState nextState = agent.evaluateNextState(game);
+        if (nextState == null) {
+            return;
+        }
+        Position lastMove = nextState.getLastMove();
+        game.play(lastMove.getRow(), lastMove.getCol());
+    }
+
+    private static Position parseUserInput(String input) {
+        String[] posInput = input.split(",");
+        if (posInput.length != 2) {
+            printInstructions();
+            return null;
+        }
+        int row, col;
+        try {
+            row = Integer.parseInt(posInput[0]);
+            col = Integer.parseInt(posInput[1]);
+        } catch (NumberFormatException e) {
+            printInstructions();
+            return null;
+        }
+        return new Position(row, col);
+    }
+
+    private static void userPlay(Scanner scanner, TicTacToeGameState game) {
+        Position userPosition;
+        while (true) {
             do {
                 String input = scanner.nextLine();
-                String[] posInput = input.split(",");
-                row = Integer.parseInt(posInput[0]);
-                col = Integer.parseInt(posInput[1]);
-            } while (!game.play(row, col));
-            game.switchPlayer();
+                userPosition = parseUserInput(input);
+            } while (userPosition == null);
 
-            if (!game.isOver()) {
-                TicTacToeGameState nextState = agent.evaluateNextState(game);
-                // check for not null
-                Position lastMove = nextState.getLastMove();
-                game.play(lastMove.getRow(), lastMove.getCol());
-                game.print();
-                game.switchPlayer();
+            try {
+                if (game.play(userPosition.getRow(), userPosition.getCol())) {
+                    return;
+                } else {
+                    System.out.printf("(%d,%d) has already been taken. ", userPosition.getRow(), userPosition.getCol());
+                    printInstructions();
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.printf("(%d,%d) is not on the board. ", userPosition.getRow(), userPosition.getCol());
+                printInstructions();
             }
         }
-        System.out.println("Game over.");
     }
+
+    private static void printGameOver(TicTacToeGameState gamestate) {
+        if (gamestate.hasWin(Player.X)) {
+            System.out.println("Player X won.");
+        } else if (gamestate.hasWin(Player.O)) {
+            System.out.println("Player O won.");
+        } else {
+            System.out.println("Game ended in a draw.");
+        }
+    }
+
+    private static void printInstructions() {
+        System.out.println("Enter '<row>,<col>' to play a position:");
+    }
+
 }
